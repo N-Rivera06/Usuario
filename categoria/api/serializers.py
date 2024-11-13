e7f762adfedf660b53168e7c11e7d97bcd81363d
N-Rivera06 <ncamayorive@gmail.com>
2024-11-12 20:10:23 -0500

    Delete categoria directory

--- categoria/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from categoria.models import Categoria
-
-class CategoriaSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Categoria
-        fields=['id_categoria','nombre_categoria','descripcion']
-        
