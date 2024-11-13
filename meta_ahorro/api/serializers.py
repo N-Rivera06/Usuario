dacc414f1f98ef0a433132198914b5393e32a418
N-Rivera06 <ncamayorive@gmail.com>
2024-11-12 20:10:39 -0500

    Delete meta_ahorro directory

--- meta_ahorro/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from meta_ahorro.models import MetaAhorro
-
-class MetaAhorroSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = MetaAhorro
-        fields = ['id_meta', 'descripcion','monto_ahorro','fecha_limite','id_user']
--- meta_ahorro/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetaAhorroConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meta_ahorro'
